7bd905164f6c9b9aabed2b0ba3d1b10322ba71fd
ORLANDO DOS SANTOS DOURADO <62121619+OrlandoSD@users.noreply.github.com>
2024-06-12 16:12:15 -0300

    commit criando a segunda tela e um botão para ativa-la

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import customtkinter as s  # importando biblioteca
+import customtkinter as ctk  # importando biblioteca
 
-janela = s.CTk() #Criando a janela
+
+janela = ctk.CTk() #Criando a janela
 
 #Configurando a janela principal
 janela.title("Cadastro de Equipamentos")
@@ -8,8 +9,16 @@ janela.geometry("700x400")
 janela.maxsize(width=900, height=550)
 janela.minsize(width=500, height=300)
 janela.resizable(width=False, height=False)
-janela.iconify()
-janela.deiconify()
 
+#Costumizando tema da nossa aplicação 
+#janela._set_appearance_mode("System")
+janela._set_appearance_mode("dark")
+
+#Criando nova janela
+def nova_tela():
+    nova_janela = ctk.CTkToplevel(janela)
+    nova_janela.geometry("300x200")
+
+btn_nova_tela = ctk.CTkButton(master=janela, text="Abrir nova Janela", command=nova_tela).place(x=300, y=100)
 
 janela.mainloop()
